49df8c94e6a3f696e41ea56b5ab7b5bdc52c2c62
keyurgandhi99 <68724854+keyurgandhi99@users.noreply.github.com>
2020-08-13 12:09:16 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 import java.util.Arrays;
-public class Program1
+public class Main
 {
 	public static void main(String[] args) 
 	{
 		
-        Program1 obj=new Program1();
+        Main obj=new Main();
         obj.quiz();
- 
+      
     }
 
 public void quiz()
 {
     
     Scanner in = new Scanner(System.in);
-        System.out.println("Give Your Login Details");	
+        System.out.println("Give Your Registartion Details");	
         System.out.println("Enter name: ");	
         String name=in.nextLine();
         while(name.length()!=0)
@@ -34,14 +34,14 @@ public void quiz()
         }
     System.out.println("welcome to "+name);
     System.out.println("Select your Subject to play Quiz");
-    System.out.println("1.Computer Science");
+    System.out.println("1.Computer Science - Press 1");
     int ch=in.nextInt();
     switch(ch)
     {
         case 1:
             System.out.println("Select Sub Topic Of Science Subject");
-            System.out.println("1.Data Structure");
-            System.out.println("2.DBMS");
+            System.out.println("1.Data Structure - press 1");
+            System.out.println("2.DBMS - Press 2");
             int ch1=in.nextInt();
             switch(ch1)
             {
@@ -55,7 +55,9 @@ public void quiz()
                     break;
                 
             }
-       
+            
+            
+        
     }
     
     
@@ -65,8 +67,8 @@ public void menu()
 {
     Scanner in1 = new Scanner(System.in);
     System.out.println("Profile");
-    System.out.println("1.Add Friend");
-    System.out.println("2.Add Quiz");
+    System.out.println("1.Add Friend - press 1");
+    System.out.println("2.Add Quiz - press 2");
     System.out.println("3.Skip");
     int ch2=in1.nextInt();
     switch(ch2)
@@ -89,25 +91,25 @@ private void addGuest()
 {
  Scanner in2 = new Scanner(System.in);
  String add_guest[]=new String[6];
- for (int i=0;i<=5;i++)
- {
+  for (int i=0;i<=5;i++)
+   {
      
-     System.out.println("Enter Guestname: ");	
+     System.out.println("Enter Friendname: ");	
    
      add_guest[i]=in2.next();
      
- }
+   }
  System.out.println(Arrays.toString(add_guest));
     
 }
 
- void addquiz()
+public void addquiz()
 {
      String Question="";
      Scanner in3 = new Scanner(System.in);
-     System.out.println("How many Question Do you want to Add");
+     System.out.println("How many Question Do you want to Add:");
      int no=in3.nextInt();
-  /*  static*/ String creator="XYZ Person";
+     String creator="XYZ Person";
  for (int i=0;i<=no;i++)
  {
     Question=in3.nextLine();
@@ -116,22 +118,31 @@ private void addGuest()
      
      
  }
- System.out.println(Question +" Created By:"+creator);
-  
+ System.out.println("Question is : " +Question+ "Created By:"+creator);
+addoption();    
+    
+    
+    
 }
 public void addoption()
 {
  Scanner in4 = new Scanner(System.in);
+ System.out.println("How many Option Do you Want to Add:");
+ int no=in4.nextInt();
 int add_option[]=new int[5];
- for (int i=0;i<=5;i++)
+System.out.println("Note:Select Numeric number for option");
+ for (int i=1;i<=4;i++)
  {
      
-     System.out.println("How many Option Do you Want to Add: ");	
+     System.out.println("Add Option:");	
    
      add_option[i]=in4.nextInt();
      
  }
  System.out.println(add_option);
 }
+
+
 }
+
 
